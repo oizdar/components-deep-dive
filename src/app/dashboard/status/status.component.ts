d85f02bb1d95bfbc7bbf4e7caf96072dac06b3a2
Radosław Malinowski <rmalinowski@booksfactory.pl>
2025-08-08 14:20:55 +0200

    class & style bindings eexcercise

--- src/app/dashboard/status/status.component.ts
+++ src/app/dashboard/status/status.component.ts
@@ -8,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './status.component.css',
 })
 export class StatusComponent {
-  currentStatus = 'online';
+  currentStatus:'online' | 'offline' | 'unknown' = 'online';
+
+  constructor() {
+    setInterval(() => {
+      const rnd = Math.random();
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.8) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000); // Simulate a status update every 5 seconds
+  }
 }
