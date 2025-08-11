a29c57c91467e7b1e2b56ac83941588d2fa14204
Radosław Malinowski <rmalinowski@booksfactory.pl>
2025-08-11 15:24:53 +0200

    templates & viewChild decorator

--- src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
@@ -1,17 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Signal, viewChild, ViewChild, ViewChildren } from '@angular/core';
 import { ButtonComponent } from "../../../shared/button/button.component";
 import { ControlComponent } from "../../../shared/control/control.component";
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
   imports: [
     ButtonComponent,
-    ControlComponent
+    ControlComponent,
+    FormsModule
   ],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
+  // @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+  // @ViewChildren(ButtonComponent) buttons?: ButtonComponent[]; for multiple items
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form'); //another way to get the form element, gives signal
+  onSubmit(title: string, request: string) {
 
+    console.log(title, request, this.form);
+    console.dir(this.form);
+    // this.form?.nativeElement.reset();
+    this.form().nativeElement.reset(); // if use signal
+  }
 }
